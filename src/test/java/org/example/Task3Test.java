15a7cf55066ac954e60c41286fb2c20c81b463d2
Suhopleshenko <aleksey.suhopleschenko@gmail.com>
2020-11-11 20:43:50 +0300

    attempt1

---
+++ src/test/java/org/example/Task3Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class Task3Test {
+    @Test
+    public void findNumber() {
+        int N = Task3Test.class.hashCode();
+    }
+}
