7b06322ebcd865434290e604b15d91cc416ca9af
Suhopleshenko <aleksey.suhopleschenko@gmail.com>
2020-11-20 13:17:44 +0300

    Homework5 исправлен счетчик циклов вкл и мессаги

--- src/main/java/Lesson5/Computer.java
+++ src/main/java/Lesson5/Computer.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class Computer {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random r = new Random();
         start();
 
     }
@@ -15,6 +13,10 @@ public class Computer {
         Scanner sc = new Scanner(System.in);
         System.out.println("What do you want to do?: \n1 - show parameters of laptop \n2 - turn on laptop \n3 - turn off laptop");
         int a = sc.nextInt();
+        if (a < 1 || a > 3) {
+            System.out.println("ERROR");
+            start();
+        }
         if (a == 1) {
             Laptop.parametersOfComputer();
         }
@@ -24,11 +26,14 @@ public class Computer {
             System.out.println("Computer burned out!");
             start();
         } else if (a == 2 && Laptop.turnOn == true) {
-            System.out.println("Computer is already working.");
+            System.out.println("Computer is already ON.");
             start();
         }
         if (a == 3 && Laptop.turnOn == false && Laptop.bernOut == false) {
-            System.out.println("Laptop is already off");
+            System.out.println("Laptop is already OFF");
+            start();
+        } else if (a == 3 && Laptop.bernOut == true && Laptop.turnOn == true) {
+            System.out.println("useless, it's already burned out!");
             start();
         } else if (a == 3 && Laptop.turnOn == true) {
             Laptop.shutdownLaptop();
--- src/main/java/Lesson5/Laptop.java
+++ src/main/java/Lesson5/Laptop.java
@@ -5,9 +5,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Laptop {
+    private static int numberOfCycle = 0;
     public static boolean turnOn = false;
     public static boolean bernOut = false;
-
     public static void parametersOfComputer() {
         int processor = 4;
         int ram = 8;
@@ -20,13 +20,13 @@ public class Laptop {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
         turnOn = true;
-        int numberOfCycle = 0;
+        //int numberOfCycle = 0;
         int b = r.nextInt(11);
         System.out.println(b);
         if (bernOut == true || numberOfCycle > 5) {
             System.out.println("Computer burned out!");
         } else {
-            if (b > 4) {                                                               // error
+            if (b > 5) {                                                               // error
                 int c = r.nextInt(2);
                 System.out.println("Error. Enter 1 or 0");
                 int n = sc.nextInt();
@@ -51,5 +51,6 @@ public class Laptop {
     public static void shutdownLaptop() {
         System.out.println("Laptop turn off");
         turnOn = false;
+        numberOfCycle++;
     }
 }
